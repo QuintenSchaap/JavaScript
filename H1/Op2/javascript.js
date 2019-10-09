a58971b1bed1cba81374a7abd7fdb02fd65ba618
QuintenSchaap <quinten.schaap@student.rocvf.nl>
2019-10-09 09:27:19 +0200

    Mijn eerste Commit

---
+++ H1/Op2/javascript.js
@@ -0,0 +1,11 @@
+var a = 10;
+
+// Loop maken van tekst
+for(var i=0; i < 10; i++) {
+   console.log("i heeft nu de waarde " + i);
+}
+
+// Tekst met max
+if (a > 9) {
+    console.log("a is groter dan 9!")
+}
---
+++ H1/Op3/opdracht3.js
@@ -0,0 +1,7 @@
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "blauw"
+}
+console.log("De kleur van mijn auto is " + mijnauto.kleur);
