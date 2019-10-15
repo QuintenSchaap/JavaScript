baff0853b8a34afc28e5fb08c7d2cf7e6d8a5008
QuintenSchaap <quinten.schaap@student.rocvf.nl>
2019-10-15 14:25:54 +0200

    H5 Af!

---
+++ H5/chooser.js
@@ -0,0 +1,41 @@
+pics = document.getElementById("pics");
+createHolders();
+ColorImages();
+
+function createHolders() {
+    for(var i = 0; i < 9; i++) {
+        pictureHolder = document.createElement("div");
+        pictureHolder.className = "colorpicture";
+        pictureHolder.id = "picture-holder-" + i;
+            pics.appendChild(pictureHolder);
+    }
+}
+
+function ColorImages() {
+    pictureHolders = document.getElementsByClassName("colorpicture");
+    for (var i = 0; i < pictureHolders.length; i++) {
+        favoriet = document.createElement("div");
+        favoriet.className = "favoriet";
+        favoriet.id = "favoriet_" + (i + 1);
+        kleurAfbeelding = document.createElement("img");
+        kleurAfbeelding.src = "img/afbeelding" + (i + 1) + ".png";
+        kleurAfbeelding.id = (i + 1);
+        kleurAfbeelding.addEventListener("click", function () {
+            maakFavoriet(this.id);
+        });
+        pictureHolders[i].appendChild(favoriet);
+        pictureHolders[i].appendChild(kleurAfbeelding);
+    }
+}
+
+function maakFavoriet(id) {
+    console.log("Maak mij Favoriet!" + id );
+    nietFavoriet = document.getElementsByClassName("favoriet");
+
+    for(var i = 0; i < nietFavoriet.length; i++) {
+        nietFavoriet[i].style.backgroundImage = "none";
+    }
+    favoriet= document.getElementById("favoriet_" + id);
+    favoriet.style.backgroundImage = "url('img/heart.png')";
+}
+
