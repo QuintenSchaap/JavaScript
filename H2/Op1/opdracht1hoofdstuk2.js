014359dda51c58f039d74fc7a865c2fe9a432fbb
QuintenSchaap <quinten.schaap@student.rocvf.nl>
2020-01-16 11:07:28 +0100

    H2 Op1 gefixt, Afbeeldingen werkten niet.

--- H2/Op1/opdracht1hoofdstuk2.js
+++ H2/Op1/opdracht1hoofdstuk2.js
@@ -1,4 +1,4 @@
-document.getElementById("titel").innerHTML = "School"
-document.getElementById("kop").innerHTML = "Lessen"
-document.getElementById("school").src = "H6.img/School.jpg"
-document.getElementById("school2").src = "H6.img/School2.jpg"
+document.getElementById("titel").innerHTML = "School";
+document.getElementById("kop").innerHTML = "Lessen";
+document.getElementById("school").src = "img/school.jpg";
+document.getElementById("school2").src = "img/school2.jpg";
